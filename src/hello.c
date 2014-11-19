2a84e8b6df1d783de1f4dfddd3821850a7d05256
zhangjl <786069240@qq.com>
2014-11-20 00:03:53 +0800

    示例程序： hello world

---
+++ src/hello.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.c
+ *
+ *    Description:  hello world
+ *
+ *        Version:  1.0
+ *        Created:  2014年11月19日 23时56分24秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  zhangjl
+ *   Organization:  FH Südwestfalen, Iserlohn
+ *
+ * =====================================================================================
+ */
+
+#include<stdio.h>
+
+int main(int argc,char* argv[])
+{
+    printf("hello world!\n");
+    return 0;
+}
+
